266271d5a7c9840419eb3e143051424abe223ee6
Alexander <alex030t@stud.kea.dk>
2022-02-20 12:26:26 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/time",(req, res)=>{
+
+    res.sendFile(__dirname+'/Public/index.html');
+});
+
+
+app.use(express.static(__dirname+'/Public'))
+app.listen(8080,()=>{
+    console.log("Now running on port: ", 8080);
+});

00f29629ffc48e94b4fb60fa2909979ac7940fba
Alexander <alex030t@stud.kea.dk>
2022-02-20 12:56:02 +0100

    Fix so teacher message only is shown tuesday

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/",(req,res)=>{
 
-    res.sendFile(__dirname+'/Public/index.html');
+    res.send("Add /time in the browser's URL")
 })
 
 
@@ -14,6 +14,6 @@ app.get("/time",(req, res)=>{
 
 
 app.use(express.static(__dirname+'/Public'))
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT||8080,()=>{
     console.log("Now running on port: ", 8080);
 });
